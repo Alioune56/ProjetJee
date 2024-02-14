d77ee752b95f9dd02ba05db9bf8b6ec54289a7e0
Alioune56 <aliounembp3k@gmail.com>
2024-02-14 12:58:14 +0000

    succes

--- src/main/java/service/TraitementUser.java
+++ src/main/java/service/TraitementUser.java
@@ -59,6 +59,8 @@ public class TraitementUser {
 		return u;
 		
 	}
+	
+	// 3.Modifier un utilisateur
 	public void Modifier(User u) {
 		// 1.Connexion Base de donnee
 		Connexion cnx = new Connexion();
@@ -67,6 +69,24 @@ public class TraitementUser {
 		// 2.Requete sql
 		String rec = "Update User set prenom='"+u.getPrenom()+"',nom='"+u.getNom()+"',email='"+u.getEmail()+"',password='"+u.getPassword()+"'"
 				+ "where id="+u.getId()+"";
+		
+		// 3.Statement
+		try {
+			cnx.st.executeUpdate(rec);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	//4.Supprimer un utilisateur
+	public void Supprimer(int id) {
+		// 1.Connexion Base de donnes
+		Connexion cnx = new Connexion();
+		cnx.connect();
+		
+		// 2.Requete sql
+		String rec = "delete User where id = '"+id+"'";
 		
 		// 3.Statement
 		try {
